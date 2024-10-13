ea82efadb707ce36ada5ea46a2d894c33dddfd8f
Oleh Riznychuk <olehriznychuk@gmail.com>
2024-10-12 23:13:59 -0400

    Add files via upload

---
+++ Tabs/script.js
@@ -0,0 +1,21 @@
+const tabs = document.querySelectorAll(".tab-btn");
+const all_content = document.querySelectorAll(".content");
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener("click", (e) => {
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    tab.classList.add("active");
+
+    let line = document.querySelector(".line");
+    line.style.width = e.target.offsetWidth + "px";
+    line.style.left = e.target.offsetLeft + "px";
+
+    all_content.forEach((content) => {
+      content.classList.remove("active");
+    });
+
+    all_content[index].classList.add("active");
+  });
+});
